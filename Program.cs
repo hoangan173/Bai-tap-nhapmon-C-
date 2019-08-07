274e78f06c381277758fc42bdf2165f46509ead4
hoangan173 <53848223+hoangan173@users.noreply.github.com>
2019-08-07 14:17:15 +0700

    Add files via upload
    Kiểm tra N có phải là số Nguyên

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int N;
+            Console.Write("Nhap N: ");
+            N = int.Parse(Console.ReadLine());
+            bool laSoNguyenTo = true;
+            if (N < 2)
+            {
+                laSoNguyenTo = false;
+            }
+            else
+            {
+                int i = 2;
+                while (i <= Math.Sqrt(N))
+                {
+                    if (N % i == 0)
+                    {
+                        laSoNguyenTo = false;
+                        break;
+                    }
+                    i = i + 1;
+                }
+            }
+            if (laSoNguyenTo)
+            {
+                Console.WriteLine($"{N} la so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine($"{N} khong phai la so nguyen to");
+            }
+            Console.ReadLine();
+        }
+    }
+}
