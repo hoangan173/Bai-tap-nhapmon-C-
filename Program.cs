a4c457de97e66b2fa9fd2fd69ee36547510a4173
hoangan173 <53848223+hoangan173@users.noreply.github.com>
2019-08-07 14:40:57 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.Write("Nhap N: ");
+            N = int.Parse(Console.ReadLine());
+            Console.Write("Day so nguyen to: ");
+            int a = 2;
+            while (a <= N)
+            {
+                bool laSoNguyenTo = true;
+                if (a < 2)
+                    {
+                    laSoNguyenTo = false;
+                } 
+                else
+                {
+                int i = 2;
+                while (i <= Math.Sqrt(a))
+                {
+                    if (a % i == 0)
+                    {
+                        laSoNguyenTo = false;
+                        break;
+                    }
+                    i = i + 1;
+                }
+                }
+                if (laSoNguyenTo)
+                {
+                Console.WriteLine($"{a} ");
+                }
+            a = a + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
